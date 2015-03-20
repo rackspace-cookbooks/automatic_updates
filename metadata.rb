323d1a1705f9582958cfbe429f945686302d5969
Martin Smith <martin@mbs3.org>
2015-03-20 08:55:14 -0500

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: utf-8
+name 'automatic_updates'
+maintainer 'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
+license 'Apache 2.0'
+description 'Installs/Configures automatic_updates'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+depends 'apt'
+depends 'yum'
