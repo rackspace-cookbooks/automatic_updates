e809fa6fd9bbcb89159d970d7ff70721e0a3f191
I Smith <ian.smith@pnnl.gov>
2017-02-22 10:52:16 -0800

    Cleanup and troubleshooting:
    1. Berksfile
      * Updating the URL used for the supermarket, was using the deprecated one
    2. metadata.rb
      * Adding the source and issue URLs per foodcritic
    3. libraries/debian_helper.rb
      * Changing the installation of the debconf-utils package to upgrade so it regenerates any default config that may have been missing and keeps the package current
      * Changing the installation of the unattended-upgrades package to upgrade so it can regenerate any default config that may have been missing and keeps the package current
      * Removing what was a lazy evaluation of the response_file because it isn’t needed and happens later than desired this way
      * The naming of the unattended upgrades variable didn’t align with other instances of the naming of the file, changed to unattended_upgrades throughout
      * Rearranged the execute resources for the rackspace perl scripts so the resources were used in a bit more explicit manner, cleaner chefspec testing this way as well
      * Removing the guard in the enable_auto_updates execute resource because it broke on Ubuntu 14.04 and it should not be a problem to enforce this on every run of chef

--- libraries/debian_helpers.rb
+++ libraries/debian_helpers.rb
@@ -6,27 +6,26 @@ module AutomaticUpdates
     module Debian
       def enable_auto_updates_debian
         package 'debconf-utils' do
-          action :install
+          action :upgrade
         end
         package 'unattended-upgrades' do
-          response_file lazy { 'unattended-upgrades.seed.erb' }
-          action :install
+          response_file 'unattended-upgrades.seed.erb'
+          action :upgrade
         end
-        unattended_upgrade_file = '/etc/apt/apt.conf.d/50unattended-upgrades'
+        unattended_upgrades_file = '/etc/apt/apt.conf.d/50unattended-upgrades'
         execute 'rackspace_allowed_origins' do
-          command "perl -pi -e 's/(Allowed-Origins\s*{)/$1\n\t\"serveragent main\";\n\t\"cloudmonitoring main\";/;' #{unattended_upgrade_file}"
+          command "perl -pi -e 's/(Allowed-Origins\s*{)/$1\n\t\"serveragent main\";\n\t\"cloudmonitoring main\";/;' #{unattended_upgrades_file}"
           only_if { rackspace? }
           action :run
         end
         # New format(Currently only Deb 7)
         execute 'rackspace_unattended_upgrade' do
-          command "perl -pi -e 's/(Unattended-Upgrade::Origins-Pattern\s*{)/$1\n\t\"o=serveragent,a=main\";\n\t\"o=cloudmonitoring,a=main\";/;' #{unattended_upgrade_file}"
+          command "perl -pi -e 's/(Unattended-Upgrade::Origins-Pattern\s*{)/$1\n\t\"o=serveragent,a=main\";\n\t\"o=cloudmonitoring,a=main\";/;' #{unattended_upgrades_file}"
           only_if { rackspace? }
           action :run
         end
         execute 'enable_auto_updates' do
           command 'echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections'
-          only_if 'debconf-get-selections | grep unattended-upgrades | grep boolean | grep false'
           notifies :run, 'execute[dpkg-reconfigure-unattended-upgrades]', :immediately
         end
         execute 'dpkg-reconfigure-unattended-upgrades' do
