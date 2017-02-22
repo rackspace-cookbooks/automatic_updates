e2b8f88ebb97e874989df76465d18da82547e596
I Smith <ian.smith@pnnl.gov>
2017-02-22 10:52:16 -0800

    Cleanup and troubleshooting:
    1. Berksfile
      * Updating the URL used for the supermarket, was using the deprecated one
    2. metadata.rb
      * Adding the source and issue URLs per foodcritic
    3. libraries/debian_helper.rb
      * Changing the installation of the debconf-utils package to upgrade so it regenerates any default config that may have been missing and keeps the package current
      * Changing the installation of the unattended-upgrades package to upgrade so it can regenerate any default config that may have been missing and keeps the package current
      * Removing what was a lazy evaluation of the response_file because it isn’t needed and happens later than desired this way
      * The naming of the unattended upgrades variable didn’t align with other instances of the naming of the file, changed to unattended_upgrades throughout
      * Rearranged the execute resources for the rackspace perl scripts so the resources were used in a bit more explicit manner, cleaner chefspec testing this way as well
      * Removing the guard in the enable_auto_updates execute resource because it broke on Ubuntu 14.04 and it should not be a problem to enforce this on every run of chef

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures automatic_updates'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/rackspace-cookbooks/automatic_updates'
+issues_url 'https://github.com/rackspace-cookbooks/automatic_updates/issues'
 version '0.2.1'
 
 depends 'apt'
